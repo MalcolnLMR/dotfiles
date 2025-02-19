438f8c8a684aa3b37dec1076ddbce2a5697848a1
MalcolnLMR <malcolnlucas@gmail.com>
2025-02-18 21:51:06 -0300

    update retroarch

--- scripts/arch/install-retroarch.sh
+++ scripts/arch/install-retroarch.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
-sudo pacman -Sy retroarch
+sudo pacman -Sy retroarch retroarch-assets-glui retroarch-assets-ozone retroarch-assets-xmb
+
+sudo pacman -S libretro-mame libretro-play libretro-snes9x libretro-picodrive libretro-mgba libretro-desmume
 
 
